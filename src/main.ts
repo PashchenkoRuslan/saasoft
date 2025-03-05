4342e4c76c6e4107014ed826443a8b6b7b8fe01c
ruslanpyou <ruslanpyou@gmail.com>
2025-03-05 17:04:08 +0500

    refactor: remove unnecessary components

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
 
-const app = createApp(App)
+const vuetify = createVuetify();
 
-app.use(createPinia())
-
-app.mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.mount("#app");
