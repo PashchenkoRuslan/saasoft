d215ae7203039ec80d0992c6d504a6d8a0de9c17
ruslanpyou <ruslanpyou@gmail.com>
2025-03-06 21:42:35 +0500

    feat: integrate Pinia for state management

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
+import "@mdi/font/css/materialdesignicons.css";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
 const vuetify = createVuetify();
+const pinia = createPinia();
 
 const app = createApp(App);
 app.use(vuetify);
+app.use(pinia);
 app.mount("#app");
